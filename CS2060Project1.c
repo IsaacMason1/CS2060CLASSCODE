d5b7c88b936fd3f88257b1afb34d9a0e033b59e4
IsaacMason1 <Imason2@uccs.edu>
2023-09-23 16:48:58 -0600

    WorkingCodeWithComentes

--- CS2060Project1.c
+++ CS2060Project1.c
@@ -1,42 +1,66 @@
 #include <stdio.h>
 
-// Constants for rental property information 
-int const SENTINEL_NEG1 = -1;
-unsigned int const MIN_RENTAL_NIGHTS = 1;/*The difference between unsigned int const and signed int 
+// Constants for rental property information
+unsigned int const MIN_RENTAL_NIGHTS = 1;/*The difference between unsigned int const and signed int
 const lies in the range of values they can represent and how they handle negative and positive numbers:
     Unsigned int is an integer data type that represents non-negative whole numbers.It cannot represent negative numbers.
         Signed int is an integer data type that can represent both positive and negative whole numbers.
 */
+
 unsigned int const MAX_RENTAL_NIGHTS = 14;
 unsigned int const INTERVAL_1_NIGHTS = 3;
 unsigned int const INTERVAL_2_NIGHTS = 6;
 double const RENTAL_RATE = 400.00;
 double const DISCOUNT = 50.00;
 
-// Function prototypes - they kind of act as an interface from java
+// Function prototypes -  they kind of act as an interface from java
 void printRentalPropertyInfo(unsigned int minNights, unsigned int maxNights, unsigned int interval1Nights, unsigned int interval2Nights, double rate, double discount);
-int getValidInt(int min, int max, int sentinel);
+int getValidInt(int min, int max);
 double calculateCharges(unsigned int nights, unsigned int interval1Nights, unsigned int interval2Nights, double rate, double discount);
 void printNightsCharges(unsigned int nights, double charges);
 
-int main() {
-    /*Print rental property information - printRentalPropertyInfo() under main will be used to
+int main() { 
+    unsigned int totalNights = 0;
+    double totalCharges = 0.0;
+    int isFirstInput = 1; // Flag to track if this is the first input
+
+      /*Print rental property information - printRentalPropertyInfo() under main will be used to
     print out the desired strings */
+
     printRentalPropertyInfo(MIN_RENTAL_NIGHTS, MAX_RENTAL_NIGHTS, INTERVAL_1_NIGHTS, INTERVAL_2_NIGHTS, RENTAL_RATE, DISCOUNT);
 
-    /* Get the number of nights from the user - getValidInt() under main will be used to find a vaild int
-     that fits the assaignment details, this is also where i will make sure that the code will not 
+    while (1) {
+           /* Get the number of nights from the user - getValidInt() under main will be used to find a vaild int
+     that fits the assaignment details, this is also where i will make sure that the code will not
      stop running fro some random reason such as a -1 or 000 being entered. */
-    int nights = getValidInt(MIN_RENTAL_NIGHTS, MAX_RENTAL_NIGHTS, SENTINEL_NEG1);
 
-    //-1 is used as a sentinel value to signal that there were no rentals when input by the user.
-    if (nights != SENTINEL_NEG1) {
+        int nights = getValidInt(MIN_RENTAL_NIGHTS, MAX_RENTAL_NIGHTS);
+
+        if (isFirstInput && nights == -1) {
+            // Handles the first input being -1 if first input is -1
+            printf("There were no rentals.\n");
+            break;//gets out of the while loop if -1 is entered
+        }
+
+        isFirstInput = 0; /* Set the flag to false after the first input, the flag willkeep track of whether this is the first input entered.
+            If the first input is -1, the program will immediately display "There were no rentals" and exit the loop.
+            After the first input, the isFirstInput flag is set to 0 (false), so the program will not consider -1 
+            as a special case sense it will no longer be entered first 
+        */
+
+        if (nights == -1) {
+            // Print the total nights and charges when -1 is entered from every entree while the while loop was running
+            printf("\nTotal Nights: %u\nTotal Charges: $%.2lf\n", totalNights, totalCharges);
+            break;
+        }
+
         // Calculate charges and print results
         double charges = calculateCharges(nights, INTERVAL_1_NIGHTS, INTERVAL_2_NIGHTS, RENTAL_RATE, DISCOUNT);
         printNightsCharges(nights, charges);
-   //more info about calc charges down bellow
-    } else {
-        printf("There were no rentals.\n");//should only ever print this is -1 is entered
+
+        // Update total nights and charges to fit the latest total
+        totalNights += nights;
+        totalCharges += charges;
     }
 
     return 0;
@@ -49,22 +73,30 @@ void printRentalPropertyInfo(unsigned int minNights, unsigned int maxNights, uns
     printf("$%.2lf discount rate a night for each remaining night over %u.\n", discount * 2, interval2Nights);
 }
 
-int getValidInt(int min, int max, int sentinel) {
+int getValidInt(int min, int max) {
     int input;
+    char buffer[256]; // Buffer to read input as a string
     while (1) {
         printf("Enter the number of nights you want to rent the property: ");
-        if (scanf("%d", &input) != 1) {
-            // Invalid input (non-integer)
+        if (fgets(buffer, sizeof(buffer), stdin) == NULL) {
+            // if error reading input acurs print statemtn bellow
+            printf("Error reading input. Please try again.\n");
+            continue;
+        }
+        if (sscanf(buffer, "%d", &input) != 1) {
+            // if invalid input (not an integer) is entered then print statement bellow
             printf("Error: Not an integer number. Please enter the value again.\n");
-            while (getchar() != '\n'); // Clear input buffer
-        } else if (input == sentinel) {
-            // Check for sentinel value
+            continue;
+        }
+        if (input == -1) {
+            // will exit loop when -1 is entered
             break;
-        } else if (input < min || input > max) {
-            // Input out of range
+        }
+        if (input < min || input > max) {
+            // if input is out of range of the min and max nights then print this statement
             printf("Error: Not within %d and %d. Please enter the value again.\n", min, max);
         } else {
-            // Valid input, break out of the loop
+            // Valid input was entered, break out of the loop
             break;
         }
     }
@@ -73,17 +105,19 @@ int getValidInt(int min, int max, int sentinel) {
 
 double calculateCharges(unsigned int nights, unsigned int interval1Nights, unsigned int interval2Nights, double rate, double discount) {
     double totalCharge = 0.0;
-    if (nights <= interval1Nights) { //this is the base pay without any discounts being added
-        totalCharge = rate * nights;
-    } else if (nights <= interval2Nights) { // this is for the 50$ disount for nights 4 to 6
+    if (nights <= interval1Nights) {
+        totalCharge = rate * nights;//this is the flat rate before getting to the 4th night discount
+    } else if (nights <= interval2Nights) {
         totalCharge = (rate * interval1Nights) + ((nights - interval1Nights) * (rate - discount));
-    } else { //and this is with the 100$ discount after night 6
+        //this is for the night 4-6 50$ discount
+    } else {
         totalCharge = (rate * interval1Nights) + ((interval2Nights - interval1Nights) * (rate - discount)) + ((nights - interval2Nights) * (rate - discount * 2));
+        //this is with all of the discounts after acheiving 7 or more nights
     }
     return totalCharge;
 }
 
-void printNightsCharges(unsigned int nights, double charges) {
+void printNightsCharges(unsigned int nights, double charges) { //strings to show the finale results
     printf("\nRental Charges\n\n");
     printf("Nights          Charge\n");
     printf("%-5u          $%.2lf\n", nights, charges);
